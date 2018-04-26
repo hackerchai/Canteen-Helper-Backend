92c9d7521735656e12b8076ce066963b5f3f4e0e
HFO4 <912394456@qq.com>
2018-04-26 20:20:00 +0800

    add websocket primary push

--- application/database.php
+++ application/database.php
@@ -15,11 +15,11 @@ return [
     // 服务器地址
     'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => 'canteen_helper',
+    'database'        => 'canteen',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'lzx2000228',
     // 端口
     'hostport'        => '',
     // 连接dsn
