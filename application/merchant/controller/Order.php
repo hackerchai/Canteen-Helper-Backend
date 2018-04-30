a3ca70dd31aa826272c3d8a9716fab58d7ea0f77
Hackerchai <hackerchai.com@gmail.com>
2018-05-01 05:48:42 +0800

    add

--- application/merchant/controller/Order.php
+++ application/merchant/controller/Order.php
@@ -1,11 +1,12 @@
 <?php
 namespace app\merchant\controller;
+use app\lib\validate\MerchantIdValidate;
 use app\student\model\Order as OrderModel;
 class Order extends BaseController
 {
 
     public function today(){
-        $ov=new OrderValidate();
+        $ov=new MerchantIdValidate();
         $param=$ov->goCheck();
         $order=new OrderService($param["token"]);
         $data=$order->create();
