d2d9bdac563e9ad226ca0d98344a21ad4e45af13
Hackerchai <hackerchai.com@gmail.com>
2018-05-20 01:10:02 +0800

    fix some problems

--- application/student/model/Address.php
+++ application/student/model/Address.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 namespace app\student\model;
 class Address extends BaseModel{
@@ -25,34 +24,4 @@ class Address extends BaseModel{
 
         return $addr;
     }
-    
-=======
-<?php
-namespace app\student\model;
-class Address extends BaseModel{
-    public function getGardenAttr($value){
-      
-        $garder=[1 => "梅园" ,2 => "竹园", 3 =>"松园", 4 => "兰园"];
-      
-        return $garder[$value];
-    }
-    public function getBuildingAttr($value){
-       
-        $garder=[1 => "一栋",2 => "二栋", 3 => "三栋", 4 => "四栋",
-        5 => "五栋", 6 => "六栋", 7 => "七栋", 8 => "八栋",  9 => "九栋"];
-        return $garder[$value];
-    }
-    public function getAddressByUid($uid){
-        $address=$this->where("uid" ,"=",$uid)->select();
-        $addr=[];
-        if(!empty($address)){
-            foreach($address as $a){
-                array_push($addr,$a);
-            }
-        }
-
-        return $addr;
-    }
-    
->>>>>>> bbdbebe6ba5cf440c32e81b1f67ab43c77fe0f1a
 }
